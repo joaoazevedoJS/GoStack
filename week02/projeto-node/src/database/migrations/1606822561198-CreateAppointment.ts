3b831dd0cd93968cf0d9512c6df448e976d390ea
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-16 21:21:29 -0300

    Week02 - Add migrations and relations

--- week02/projeto-node/src/database/migrations/1606822561198-CreateAppointment.ts
+++ week02/projeto-node/src/database/migrations/1606822561198-CreateAppointment.ts
@@ -9,7 +9,7 @@ export default class CreateAppointment1606822561198
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
