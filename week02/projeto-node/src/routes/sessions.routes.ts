b226016a205ab6aba15162dce5750cd84ec13927
Joao Azevedo <devjoaoazevedo@gmail.com>
2020-12-27 13:46:55 -0300

    Week 02 - finished

--- week02/projeto-node/src/routes/sessions.routes.ts
+++ week02/projeto-node/src/routes/sessions.routes.ts
@@ -5,19 +5,15 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({ email, password });
+  const { user, token } = await authenticateUser.execute({ email, password });
 
-    const userWithoutPassword = { ...user, password: undefined };
+  const userWithoutPassword = { ...user, password: undefined };
 
-    return response.json({ user: userWithoutPassword, token });
-  } catch (error) {
-    return response.status(400).json({ message: error.message });
-  }
+  return response.json({ user: userWithoutPassword, token });
 });
 
 export default sessionsRouter;
--- week02/projeto-node/src/routes/users.routes.ts
+++ week02/projeto-node/src/routes/users.routes.ts
@@ -11,19 +11,15 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    const userWithoutPassword = { ...user, password: undefined };
+  const userWithoutPassword = { ...user, password: undefined };
 
-    return res.json(userWithoutPassword);
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
+  return res.json(userWithoutPassword);
 });
 
 usersRouter.patch(
@@ -31,20 +27,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      const userWithoutPassword = { ...user, password: undefined };
+    const userWithoutPassword = { ...user, password: undefined };
 
-      return response.json({ user: userWithoutPassword });
-    } catch (err) {
-      return response.status(400).json({ message: err.message });
-    }
+    return response.json({ user: userWithoutPassword });
   },
 );
 
