870805d1242d0601c7d76af649920a18a45e728c
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-02-01 07:55:23 -0300

    Add routes and fonts

--- week03/gobarberapp/src/App.tsx
+++ week03/gobarberapp/src/App.tsx
@@ -1,6 +1,20 @@
-import React, {FC} from 'react';
-import {View} from 'react-native';
+import 'react-native-gesture-handler';
 
-const App: FC = () => <View />;
+import React, { FC } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { StatusBar, View } from 'react-native';
+
+import Routes from './routes';
+
+const App: FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 
 export default App;
---
+++ week03/gobarberapp/src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+import { Container } from './styles';
+
+const SignUp: FC = () => {
+  return <Container />;
+};
+
+export default SignUp;
