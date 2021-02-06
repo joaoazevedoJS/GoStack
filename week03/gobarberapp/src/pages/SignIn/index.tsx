66686949c3bc2eef12e3d012c7e9a4167c79a78c
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-02-06 16:01:51 -0300

    Add components and signup page

--- week03/gobarberapp/src/pages/SignIn/index.tsx
+++ week03/gobarberapp/src/pages/SignIn/index.tsx
@@ -1,18 +1,85 @@
-import React, { FC } from 'react';
-
-import { Image } from 'react-native';
-
-import { Container, Title } from './styles';
+import React, { FC, useCallback, useRef } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.png';
 
-const SignIn: FC = () => {
-  return (
-    <Container>
-      <Image source={logoImg} />
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-      <Title>Faça seu logon</Title>
-    </Container>
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+const SignIn: FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Form ref={formRef} onSubmit={handleSignIn}>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                /* */
+              }}
+            >
+              Entrar
+            </Button>
+          </Form>
+
+          <ForgotPassword
+            onPress={() => {
+              formRef.current?.submitForm();
+            }}
+          >
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </ScrollView>
   );
 };
 
--- week03/gobarberapp/src/pages/SignUp/index.tsx
+++ week03/gobarberapp/src/pages/SignUp/index.tsx
@@ -1,9 +1,61 @@
 import React, { FC } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flex: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Crie sua conta</Title>
+          </View>
+
+          <Input name="name " icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button
+            onPress={() => {
+              /* */
+            }}
+          >
+            Entrar
+          </Button>
+        </Container>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </ScrollView>
+  );
 };
 
 export default SignUp;
