08df91b74d5c4dc03c3f8817e0a83bab8de3d888
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-02-16 20:32:50 -0300

    Week 04 - add tests

---
+++ week04/projeto-node/src/modules/appointments/test/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(2 + 2).toBe(4);
+  // });
+});
